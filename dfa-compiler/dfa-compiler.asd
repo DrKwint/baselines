f3c78d90528ef883943f50e5aa9280d945e87f24
Samuel W. Flint <swflint@flintfam.org>
2019-10-13 18:38:53 -0500

    Include options handling and repl/swank starting for development

--- dfa-compiler/dfa-compiler.asd
+++ dfa-compiler/dfa-compiler.asd
@@ -6,13 +6,16 @@
   :license  "Specify license here"
   :version "0.0.1"
   :serial t
-  :depends-on (:uiop)
+  :depends-on (:uiop
+               :unix-opts
+               :prepl
+               :swank)
   :components ((:file "package")
                (:file "regexp")
                (:file "dfa-compiler"))
   :build-operation "program-op"
   :build-pathname "dfa-compiler"
-  :entry-point "dfa-compiler:compiler-temp-hello-world")
+  :entry-point "dfa-compiler:start-dfa-compiler")
 
 #+sb-core-compression
 (defmethod asdf:perform ((o asdf:image-op) (c asdf:system))
--- dfa-compiler/dfa-compiler.lisp
+++ dfa-compiler/dfa-compiler.lisp
@@ -2,6 +2,56 @@
 
 (in-package #:dfa-compiler)
 
-(defun compiler-temp-hello-world (&rest args)
-  (format t "Hello World!~%")
-  (uiop:quit 0 t))
+(defun start-dfa-compiler ()
+  (dfa-compiler (argv0) (command-line-arguments)))
+
+(define-opts
+  (:name :help
+   :description "Show Help"
+   :short #\h
+   :long "help")
+  (:name :repl
+   :description "Open REPL with packages loaded."
+   :long "repl")
+  (:name :swank
+   :description "Start swank on PORT."
+   :long "swank"
+   :arg-parser #'parse-integer
+   :meta-var "PORT"))
+
+(defun dfa-compiler (program-name program-arguments)
+  (multiple-value-bind (options free-arguments)
+      (handler-case (get-opts)
+        (unknown-option (condition)
+          (format *error-output*
+                  "~&ERROR: Unknown option \"~A\".~%"
+                  (option condition))
+          (quit 1 t))
+        (missing-arg (condition)
+          (format *error-output*
+                  "~&ERROR: Option \"~A\" is missing an argument.~%"
+                  (option condition)) 
+          (quit 1 t))
+        (arg-parser-failed (condition)
+          (format *error-output*
+                  "~&ERROR: Option \"~A\" is unable to read its argument.~%"
+                  (option condition))
+          (quit 1 t))
+        (missing-required-option (condition)
+          (format *error-output*
+                  "~&ERROR: Missing the following required options:~%~{~3T--~A~^~%~}~%"
+                  (mapcar #'long (missing-options condition)))
+          (quit 1 t)))
+    
+    (unless (null free-arguments)
+      (format *error-output* "~&Too many additional arguments!~%")
+      (quit 1 t))
+    (when (getf options :repl)
+      (format t "~&DFA Compile REPL.~%~%")
+      (repl :nobanner t))
+    (when (getf options :swank)
+      (create-server :dont-close t
+                     :port (getf options :swank))
+      (repl :nobanner t))
+    )
+  (quit 0 t))
