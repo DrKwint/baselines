e9e2655947a816181c7cb869ae71d0071c68c449
Samuel W. Flint <swflint@flintfam.org>
2019-10-13 16:23:57 -0500

    Add a number of packages

--- dfa-compiler/package.lisp
+++ dfa-compiler/package.lisp
@@ -3,3 +3,34 @@
 (defpackage #:dfa-compiler
   (:use #:cl)
   (:export #:compiler-temp-hello-world))
+
+(defpackage #:dfa-compiler.representations.dfa
+  (:use #:cl)
+  (:nicknames #:dfa))
+
+(defpackage #:dfa-compiler.representations.nfa
+  (:use #:cl)
+  (:nicknames #:nfa))
+
+(defpackage #:dfa-compiler.representations.regexp
+  (:use #:cl)
+  (:nicknames #:regexp))
+
+(defpackage #:dfa-compiler.read-regexp
+  (:use #:cl
+        #:regexp))
+
+(defpackage #:dfa-compiler.conversions.regexp-nfa
+  (:use #:cl
+        #:regexp
+        #:nfa))
+
+(defpackage #:dfa-compiler.conversions.nfa-dfa
+  (:use #:cl
+        #:nfa
+        #:dfa))
+
+(defpackage #:dfa-compiler.dfa-minimizer
+  (:use #:cl
+        #:dfa))
+
