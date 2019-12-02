5e63274d7c48e65a8b4e6741e39c35200b04b7cb
Samuel W. Flint <swflint@flintfam.org>
2019-12-02 06:15:53 -0600

    Begin implementation of powerset construction

---
+++ dfa-compiler/powerset-construction.lisp
@@ -0,0 +1,15 @@
+;;;; powerset-construction.lisp
+;;;;
+;;;; Copyright (c) 2019 Samuel W. Flint <swflint@flintfam.org>
+
+(in-package #:nfa-dfa)
+
+;;; "nfa-dfa" goes here.
+
+;; (defun subset-construction (nfa)
+;;   (let* ((q0 (epsilon-closure (start-state nfa)))
+;;          (q q0)
+;;          (worklist (list q0)))
+;;     ()))
+
+;;; End nfa-dfa
