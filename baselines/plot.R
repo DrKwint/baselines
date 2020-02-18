12fbad4256dac946131214ce36394afa66d14360
Samuel W. Flint <swflint@flintfam.org>
2020-02-18 11:30:55 -0600

    Add notes about baselines files

--- baselines/plot.R
+++ baselines/plot.R
@@ -5,6 +5,14 @@ library("tidyverse")
 library("munsell")
 library("RcppCNPy")
 
+## Files -- all per timestamp, 0 to and including 1 is episode
+## thing_rew_mod.npy -- Reward Mod, float
+## viols.npy -- constraint violations, binary
+## action.npy -- integer valued
+## done.npy -- episodes
+## raw_reward.npy -- Reward get before modification, clipped
+## reward.npy -- Reward after modification, clipped
+
 ## Episode Accumulation
 
 ## Rolling Window
