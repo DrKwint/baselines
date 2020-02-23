966e82fb7d1e2e3309f8e843c0cbaf6b08759446
Samuel W. Flint <swflint@flintfam.org>
2020-02-22 20:07:29 -0600

    Start implementing nfa-to-dfa

--- dfa-compiler/powerset-construction.lisp
+++ dfa-compiler/powerset-construction.lisp
@@ -12,4 +12,21 @@
 ;;          (worklist (list q0)))
 ;;     ()))
 
+(defun nfa-to-dfa (nfa)
+  (let* ((new-start (make-instance '<dfa-multi-node> :label 'dfa-start
+                                                     :involved-nfa-nodes (epsilon-closure (slot-value nfa 'start))))
+         (dfa (make-instance '<dfa>
+                             :start new-start)))
+    ;; q_0 \gets epsilon-closure(nfa_start)
+    ;; Q \gets q_0
+    ;; WorkList \gets {q_0}
+    ;; while (Worklist \neq NULL):
+    ;;    remove q from WorkList
+    ;;    For each char c in Sigma:
+    ;;    t \gets epsilon-closure(Delta(Q, c))
+    ;;    T[q, c] \gets t
+    ;;    if t \neq Q, add t to Q and WorkList
+
+    ))
+
 ;;; End nfa-dfa
